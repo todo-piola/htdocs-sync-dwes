26758f33569261d5a6205f85bde398de2a22e078
Franco Benavides <benavidesgarciafranco@gmail.com>
2025-10-11 13:01:09 +0200

    Añadidos form ficheros intermedio

---
+++ 2Scripts/04-origen_lvl_intermedio.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Formulario - Origen</title>
+</head>
+<body>
+    <h1>Formulario simple</h1>
+
+    <form action="destino_lvl_intermedio.php" method="post">
+        <label>Nombre:</label>
+        <input type="text" name="nombre" required><br><br>
+
+        <label>Teléfono:</label>
+        <input type="text" name="telefono" required><br><br>
+
+        <input type="submit" value="Enviar">
+    </form>
+</body>
+</html>
