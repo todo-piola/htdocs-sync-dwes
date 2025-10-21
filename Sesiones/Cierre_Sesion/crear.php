6f180bc7ceef3b014263b646ee0b4fb49e0ae7d0
Franco Benavides <benavidesgarciafranco@gmail.com>
2025-10-21 13:35:25 +0200

    Finalizado ejercicio

--- Sesiones/Cierre_Sesion/crear.php
+++ Sesiones/Cierre_Sesion/crear.php
@@ -4,7 +4,7 @@ session_start(); // Siempre lo primero
 // Guardamos variables y un array
 $_SESSION['usuario'] = "Franco";
 $_SESSION['edad'] = 30;
-$_SESSION['aficion'] = "futbol";
+$_SESSION['aficiones'] = ["futbol", "videojuegos", "pádel"];
 
 // Guardamos el momento en que empieza la sesión
 $_SESSION['inicio'] = time();
@@ -19,9 +19,6 @@ $_SESSION['inicio'] = time();
 </head>
 <body>
     <h2> Sesión creada correctamente</h2>
-    <p>Usuario: <?= $_SESSION['usuario'] ?></p>
-    <p>Edad: <?= $_SESSION['edad'] ?></p>
-    <p>Afición 1: <?= $_SESSION['aficion'] ?></p>
     <a href="ver.php">Ver datos de sesión</a>
 </body>
 </html>
--- Sesiones/Cierre_Sesion/ver.php
+++ Sesiones/Cierre_Sesion/ver.php
@@ -6,8 +6,18 @@ if (isset($_SESSION['usuario'])) {
     echo "<h2> Datos de la sesión actual</h2>";
     echo "Usuario: " . $_SESSION['usuario'] . "<br>";
     echo "Edad: " . $_SESSION['edad'] . "<br>";
-    echo "Primera afición: " . $_SESSION['aficiones'][0] . "<br>";
+    echo "<h3>Aficiones:</h3>";
+        foreach ($_SESSION['aficiones'] as $aficion) {
+            echo "- $aficion<br>";
+        }
+        
     echo "<a href='cerrar.php'>Cerrar sesión</a>";
+
+    $session_id = session_id(); // ID único de la sesión
+    $session_file = session_save_path() . "/sess_" . $session_id;
+
+    echo "<p>ID de sesión: $session_id</p>";
+    echo "<p>Archivo de sesión: $session_file</p>";
 } else {
     echo " No hay sesión activa.";
 }
