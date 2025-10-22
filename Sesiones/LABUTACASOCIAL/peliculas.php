380f06137fecc5ba8cd80a9379f4e81d2d84b888
Franco Benavides <benavidesgarciafranco@gmail.com>
2025-10-22 13:28:49 +0200

    Web películas con contador

---
+++ Sesiones/LABUTACASOCIAL/peliculas.php
@@ -0,0 +1,35 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Elige la película</title>
+    <link rel="stylesheet" href="estilo.css">
+</head>
+<body>
+    <header id="encabezado">
+        <div id="logo">
+            <img src="imgs/labutacasocial.png" class="logo_aedo">
+        </div>
+        <nav id="navegacion">
+            <h1 id="titulo">Bienvenid@ a Cines Hanns</h1>
+        </nav>
+    </header>
+    <section>
+        <div id="peliculas">
+            <h2> Película 1: Kimetsu </h2>
+            <img src="imgs/kimetsu_peli.webp" class="peli_img">
+        </div>
+        <div id="peliculas">
+            <h2> Película 2:  Chainsawnman </h2>
+            <img src="imgs/chainsawman_peli.webp" class="peli_img">
+        </div>
+        <div id="peliculas">
+            <h2> Película 3: Django </h2>
+            <img src="imgs/django_peli.jpg" class="peli_img">
+        </div>
+    </section>
+</body>
+</html>
