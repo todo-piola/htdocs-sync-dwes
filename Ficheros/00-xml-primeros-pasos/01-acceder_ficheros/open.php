25b06a7e317e47e3234cef70128c6e9c775e6e57
Franco Benavides <benavidesgarciafranco@gmail.com>
2025-11-05 11:50:21 +0100

    Añado php con fopen

---
+++ Ficheros/00-xml-primeros-pasos/01-acceder_ficheros/open.php
@@ -0,0 +1,18 @@
+<?php
+$fich = fopen("fichero_ejemplo.txt", "r");
+
+if ($fich === FALSE) {
+    echo "No se encuentra fichero_ejemplo.txt<br>";
+} else{
+    echo "fichero_ejemplo.txt se abrió con éxito<br>";
+    fclose($fich);
+}
+
+$fich = fopen("fichero_no_existe.txt", "r");
+
+if ($fich === FALSE) {
+    echo "No se encuentra fichero_no_existe.txt<br>";
+} else {
+    echo " fichero_no_existe.txt se abrió con éxito<br>";
+    fclose($fich);
+}
