c80d6b833e71715c679ecfe7d633a39cd6ff7391
Franco Benavides <benavidesgarciafranco@gmail.com>
2025-11-07 10:58:25 +0100

    Nuevas comprobaciones assert en tests

--- phputil/tests/CalculadoraTest.php
+++ phputil/tests/CalculadoraTest.php
@@ -6,27 +6,45 @@ class CalculadoraTest extends TestCase
 {
     public function testSuma()
     {
-        $this->assertEquals(7, Calculadora::sumar(3, 4));
+        $resultado = Calculadora::sumar(3, 4);
+        $this->assertEquals(7, $resultado);
+
+        // Nuevos asserts
+        $this->assertLessThan(10, $resultado, 'La suma debería ser menor que 10');
+        $this->assertGreaterThan(5, $resultado, 'La suma debería ser mayor que 5');
     }
 
     public function testResta()
     {
-        $this->assertEquals(1, Calculadora::restar(5, 4));
+        $resultado = Calculadora::restar(5, 4);
+        $this->assertEquals(1, $resultado);
+        $this->assertLessThan(5, $resultado, 'El resultado debería ser menor que 5');
     }
 
     public function testMultiplicacion()
     {
-        $this->assertEquals(20, Calculadora::multiplicar(5, 4));
+        $resultado = Calculadora::multiplicar(5, 4);
+        $this->assertEquals(20, $resultado);
+        $this->assertGreaterThan(10, $resultado, 'El resultado debería ser mayor que 10');
     }
 
     public function testDivision()
     {
-        $this->assertEquals(2, Calculadora::dividir(10, 5));
+        $resultado = Calculadora::dividir(10, 5);
+        $this->assertEquals(2, $resultado);
+        $this->assertLessThan(3, $resultado, 'El resultado debería ser menor que 3');
     }
 
     public function testDivisionPorCero()
     {
         $this->expectException(InvalidArgumentException::class);
         Calculadora::dividir(10, 0);
+    }
+
+    public function testNombreClase()
+    {
+        $nombre = 'Calculadora PHP';
+        // assertStringStartsWith → el nombre debe comenzar con "Calculadora"
+        $this->assertStringStartsWith('Calculadora', $nombre, 'El nombre debe empezar por "Calculadora"');
     }
 }
