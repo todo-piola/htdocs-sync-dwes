7ee037165169233177e8e8ec622d62f5527bcc25
Franco Benavides <benavidesgarciafranco@gmail.com>
2025-11-11 13:31:16 +0100

    Conexión a la bbdd exitosa y creación tabla albumes

---
+++ BBDD/01-albumes_musica/conexion.php
@@ -0,0 +1,15 @@
+<?php
+$server = "localhost";
+$user = "root";
+$password = "";
+$db = "chinese_man_db";
+try {
+ $conexion = new PDO("mysql:host=$server;dbname=$db;charset=utf8", $user,
+$password);
+ $conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+ echo "Conexión exitosa" . "<br>" ;
+} catch (PDOException $e) {
+ die("Connection failed: " . $e->getMessage());
+}
+
+?>
