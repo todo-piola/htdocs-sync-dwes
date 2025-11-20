a68cd1f8229d444dacda782f300961e7af6f98ad
Franco Benavides <benavidesgarciafranco@gmail.com>
2025-11-20 19:40:32 +0100

    Actualizo apuntes con funciones de ficheros

--- 3_Ficheros/01.1-practica_funciones_ficheros/funciones_ficheros.php
+++ 3_Ficheros/01.1-practica_funciones_ficheros/funciones_ficheros.php
@@ -5,6 +5,13 @@
     fseek, ftell, rewind, y unlink.
 */
 
+// Escribe el contenido del segundo parámetro en un fichero del primer parámetro que crea
+// un fichero, FILE_APPEND no lo sobreescribe
+file_put_contents($fichero, $contenido, FILE_APPEND);
+
+// Devuelve true si el fichero existe y false si no existe
+file_exists($fichero)
+
 $nombre = "funciones_ficheros.txt";
 
 // Crear un fichero y escribir contenido
